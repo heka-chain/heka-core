89659f16746895ff26bd935b6f374598209987bf
phantomk <phantomk94@gmail.com>
2018-10-21 17:30:15 +0800

    update: test and doc 1. add LICENSE 2. add jest test

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 heka-core for heka chain
 ---
 heka chain is a ongoing blockchain written in Typescript/Javascript
+
+## ⚠️⚠️⚠️⚠️⚠️⚠️
+This project is under construction. Please do not use it
+
+## License
+[Apache-2.0](LICENSE)
--- jest.config.js
+++ jest.config.js
@@ -1,11 +1,8 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
-  moduleNameMapper: {
-    '^@App/(.*)$': '<rootDir>/src/$1',
-    '^lib/(.*)$': '<rootDir>/common/$1'
-  },
-  "testPathDirs": [
-    "./test"
+  coverageDirectory: './coverage',
+  roots: [
+    './test'
   ]
 };
